240fc898e1708a4e82241be81bb2c750a1d34075
Z_Sarsenov <Alikhan*2016>
2023-05-09 14:59:14 +0500

    Commit 2

--- test.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-URL = 'https://hh.kz/search/vacancy?text=Swift+junior&from=suggest_post&salary=&ored_clusters=true'
-
-for page in range(0:0):
-    print(f'page={page}')
--- test2.py
+++
@@ -1,3 +0,0 @@
-a = {'a': 1, 'b': 2, 'c': 3}
-b = a.get('a')
-print(a.get('r'))
